6a31ac12135d14c687d7e926ec77a494e2b69cc9
Colby Saxton <colbyasaxton@gmail.com>
2026-08-27 13:36:29 -0400

    Add Contact class with name and phone fields

---
+++ Contact.java
@@ -0,0 +1,21 @@
+public class Contact {
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public String toString() {
+        return "Ada Lovelace | +1 617 555 0101";
+    }
+}
